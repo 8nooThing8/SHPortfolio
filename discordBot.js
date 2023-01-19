26c36222171dca2d6f461f5ed9a3fd7358d25ce4
8nooThing8 <93975705+8nooThing8@users.noreply.github.com>
2023-01-19 14:39:05 +0100

    commit

--- discordBot.js
+++ discordBot.js
@@ -16,6 +16,6 @@ client.on('ready', () => {
 
 function sendMessage() {
       const request = new XMLHttpRequest();
-      request.open("POST", "https://discord.com/api/webhooks/1065623932796940339/jPGfG6IkWF9xH8Fca8i21YBxUv2wbks8Fj25yoZ54uN061si0ldu0L3MocwyGWml_FmF");
+      request.open("POST", "");
       // replace the url in the "open" method with yours
 }
