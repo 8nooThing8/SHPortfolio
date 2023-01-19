5a44f7d167340e3f1c6fe08baf79179dff2cf6f3
8nooThing8 <93975705+8nooThing8@users.noreply.github.com>
2023-01-19 14:34:06 +0100

    coomit

--- discordBot.js
+++ discordBot.js
@@ -12,7 +12,10 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+
 function sendMessage() {
-  const channel = client.channels.cache.get('1013835199752183831');
-  channel.send('Hello everyone');
+      const request = new XMLHttpRequest();
+      request.open("POST", "https://discord.com/api/webhooks/1065623932796940339/jPGfG6IkWF9xH8Fca8i21YBxUv2wbks8Fj25yoZ54uN061si0ldu0L3MocwyGWml_FmF");
+      // replace the url in the "open" method with yours
 }
