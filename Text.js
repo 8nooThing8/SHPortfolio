113bc175535c598bde6a1652bdebab8d673fdab1
8nooThing8 <93975705+8nooThing8@users.noreply.github.com>
2023-03-15 11:16:47 +0100

    new feature

--- Text.js
+++ Text.js
@@ -1,28 +1,21 @@
-let intervalId;
-const endTime = new Date("2022-11-20T00:00:00"); // predefined end time for the countdown
-let timeLeft;
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
-function checkTime() {
-    let currentTime = new Date();
-    if (currentTime > endTime) {
-        timeLeft = 0;
-    } else {
-        timeLeft = (endTime - currentTime) / 1000; // convert to seconds
-    }
-    button.innerText = timeLeft;
-    if (timeLeft <= 0) {
-        button.innerText = 'Time is up!';
-        button.disabled = false;
-        clearInterval(intervalId); // stop the interval when time is up
-    }
-}
+client.login('MTA2NTYwNDIyMTI1ODQzNjY4OA.GZk2M' + 'v.NB9frUEeU-TXouvQZam_ATIZDhKVBzfmXmO6lE');
 
-const button = document.getElementById('countdown-button');
-button.addEventListener('click', function(){
-    if (!intervalId) {
-        intervalId = setInterval(checkTime,1000);
-    } else {
-        clearInterval(intervalId);
-        intervalId = null;
-    }
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+client.on('message', message => {
+  if (message.content === "a") {
+console.log(`read`);
+  }
 });
