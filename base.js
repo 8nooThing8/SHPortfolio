fca6916bd5fba295a52fbd1b1c773f1d52800e46
8nooThing8 <93975705+8nooThing8@users.noreply.github.com>
2025-03-24 23:54:03 +0100

    new about me and grapple ascent game added

--- base.js
+++ base.js
@@ -2,8 +2,11 @@ document.addEventListener('scroll', ScrollEvents)
 
 function MoveBackgroundSlowlyWithScroll() {
     const container = document.body;
-    container.style.backgroundPositionY = (window.scrollY / 3) * -1 + "px";
+    container.style.backgroundPositionY = (window.scrollY / 5) * -1 + "px";
 }
+
+var mysiwndowOpen;
+var myWindow;
 
 var elementMove;
 var currentPosition = 0;
@@ -13,12 +16,24 @@ var menuItems = document.getElementsByClassName("menu-item");
 
 menuItems[0].addEventListener('click', About);
 menuItems[1].addEventListener('click', Games);
+menuItems[2].addEventListener('click', Awards);
 
 function About() {
     document.getElementById("title-textID").scrollIntoView();
 }
 function Games() {
     document.getElementById("games-titleID").scrollIntoView();
+}
+function Awards() {
+    myWindow = window.open("https://Zultoox.online", "Windo", "width=500,height=400,left=1000,top=0");
+    mysiwndowOpen = true;
+}
+
+function MoveNewWindow() {
+    if (mysiwndowOpen) {
+
+    myWindow.moveBy(50, 25);
+    }
 }
 
 
@@ -37,3 +52,4 @@ function ScrollEvents() {
 }
 
 setInterval(Move, 1);
+setInterval(MoveNewWindow, 1);
